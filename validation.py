d9aabdfcf474055a95d4f6966d93410b89294e00
MichaelSolotky <mihlushi@yandex.ru>
2017-07-25 11:41:19 +0300

    Changed trimming of str

--- validation.py
+++ validation.py
@@ -1,13 +1,15 @@
 import os
-import re
 
 
 def read_file(file):
     content = set()
     for line in file:
-        list = re.split("\W+", line)[:3]
-        if list[0]:  # crutch
-            content.add(tuple(list))
+        list = line.split(",")[:3]
+        if len(list) >= 3:
+            for i in range(1):
+                list[i] = list[i].strip()
+            if list[2] != "pos" and list[2] != "neg":
+                content.add(tuple(list))
     return content
 
 
@@ -33,19 +35,26 @@ def validate(solution_number=0):
     6. F-measure on negative sample
     """
     if solution_number != 0:
-        solution_file = open("solutions/solution" + str(solution_number) + ".txt", "r")
-        answer_file = open("train/art" + str(solution_number) + ".opin.txt", "r")
-        return compare(solution_file, answer_file)
+        solution_file_path = "solutions/solution" + str(solution_number) + ".txt"
+        answer_file_path = "train/art" + str(solution_number) + ".opin.txt"
+        if os.path.isfile(solution_file_path) and os.path.isfile(answer_file_path):
+            solution_file = open(solution_file_path, "r")
+            answer_file = open(answer_file_path, "r")
+            return compare(solution_file, answer_file)
+        else:
+            return None
     else:
         num_of_solutions = 40
         result = []
         for i in range(num_of_solutions):
             solution_file_path = "solutions/solution" + str(i + 1) + ".txt"
-            if os.path.isfile(solution_file_path):
+            answer_file_path = "train/art" + str(i + 1) + ".opin.txt"
+            if os.path.isfile(solution_file_path) and os.path.isfile(answer_file_path):
                 solution_file = open(solution_file_path, "r")
-                answer_file = open("train/art" + str(i + 1) + "opin.txt", "r")
+                answer_file = open(answer_file_path, "r")
                 result.append(compare(solution_file, answer_file))
         return result
 
 
 validate(14)
+
