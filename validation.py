88ff5983c6de458f8d44cbd29712c8e738cb007b
MichaelSolotky <mihlushi@yandex.ru>
2017-07-25 20:37:15 +0300

    Finished validator module

--- validation.py
+++ validation.py
@@ -1,60 +1,58 @@
 import os
+from collections import namedtuple
 
 
 def read_file(file):
+    """Parse file
+    
+    It takes file as input, parse into lines and entities in lines
+    and returns a list of tuples (entity, entity, sentiment)
+    """
     content = set()
     for line in file:
-        list = line.split(",")[:3]
-        if len(list) >= 3:
-            for i in range(1):
+        len_of_line = 3
+        list = line.split(",")[:len_of_line]
+        if len(list) >= len_of_line:
+            for i in range(len_of_line):
                 list[i] = list[i].strip()
-            if list[2] != "pos" and list[2] != "neg":
+            if list[len_of_line - 1] == "pos" or list[len_of_line - 1] == "neg":
                 content.add(tuple(list))
     return content
 
 
-def compare(solution_file, answer_file):
-    our_answers = read_file(solution_file)
-    correct_answers = read_file(answer_file)
-    print(correct_answers)
-    # Here all the metrics will be calculated
-
-
-def validate(solution_number=0):
-    """Check quality of your solution.
+def validate(set_of_doc_numbers):
+    """Check quality of the solution.
     
-    This function computes several quality metrics of your solution and returns tuple.
-    You can optionally set a solution number. By default it compares and checks all the
-    existing solution, which are stored in 'Solutions' directory, in this case a list of tuples will be returned.
+    This function takes a set of numbers of documents as input, computes several quality metrics of the solution
+    and returns a namedtuple with them.
     Computed metrics are
-    1. precision on positive sample
-    2. recall on positive sample
-    3. F-measure on positive sample
-    4. precision on negative sample
-    5. recall on negative sample
-    6. F-measure on negative sample
+    1. precision
+    2. recall
+    3. F1-measure
     """
-    if solution_number != 0:
-        solution_file_path = "solutions/solution" + str(solution_number) + ".txt"
-        answer_file_path = "train/art" + str(solution_number) + ".opin.txt"
+    true_positives = 0
+    precision_denominator = 0
+    recall_denominator = 0
+
+    for i in set_of_doc_numbers:
+        solution_file_path = "solutions/solution" + str(i) + ".txt"
+        answer_file_path = "train/art" + str(i) + ".opin.txt"
         if os.path.isfile(solution_file_path) and os.path.isfile(answer_file_path):
             solution_file = open(solution_file_path, "r")
             answer_file = open(answer_file_path, "r")
-            return compare(solution_file, answer_file)
-        else:
-            return None
-    else:
-        num_of_solutions = 40
-        result = []
-        for i in range(num_of_solutions):
-            solution_file_path = "solutions/solution" + str(i + 1) + ".txt"
-            answer_file_path = "train/art" + str(i + 1) + ".opin.txt"
-            if os.path.isfile(solution_file_path) and os.path.isfile(answer_file_path):
-                solution_file = open(solution_file_path, "r")
-                answer_file = open(answer_file_path, "r")
-                result.append(compare(solution_file, answer_file))
-        return result
+            our_answers = read_file(solution_file)
+            correct_answers = read_file(answer_file)
+            true_positives += len(our_answers & correct_answers)
+            precision_denominator += len(our_answers)
+            recall_denominator += len(correct_answers)
+
+    estimation = namedtuple("estimation", ["precision", "recall", "f1_measure"])
+    precision = true_positives / precision_denominator
+    recall = true_positives / recall_denominator
+    f1_measure = 2 / (1 / precision + 1 / recall)
+    return estimation(precision, recall, f1_measure)
 
 
-validate(14)
-
+s = set()
+s.add(1)
+print(validate(s))
